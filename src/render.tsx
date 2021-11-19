33da93de6dc6c62ef0988601b76a4ecf314f9d76
Enrique Madrid <emadridm@gmail.com>
2021-11-19 08:21:20 -0500

    backup

--- src/render.tsx
+++ src/render.tsx
@@ -2,9 +2,12 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const RenderApp: React.FC = () => {
-    return <h2>Hello React</h2>;
-}
-
+  return (
+    <>
+      <h1>Hello React</h1>
+      <h2>jajaja</h2>
+    </>
+  );
+};
 
 ReactDOM.render(<RenderApp />, document.getElementById("root"));
-
